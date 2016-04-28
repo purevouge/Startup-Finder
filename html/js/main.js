bbeed43181b450f6c070cb407d6a0e63209cc9f5
ehab ahmed <purevouge@yahoo.com>
2016-04-29 00:46:39 +0200

    grid from csv

--- html/js/main.js
+++ html/js/main.js
@@ -86,3 +86,24 @@ function addMarker(latitude, longitude, companyName, founder, city, country, pos
 		markers[markerIndex].setIcon(selectedIcon);
 	});
 }
+/*** data tables ***/
+d3.text("data/data.csv", function (datasetText) {
+    var rows = d3.csv.parseRows(datasetText);
+    var tbl = d3.select("#data_grid").append("table");
+    // headers
+    tbl.append("thead").append("tr")
+        .selectAll("th")
+        .data(rows[0])
+        .enter().append("th")
+        .text(function(d) {
+            return d;
+        });
+    // data
+    tbl.append("tbody")
+        .selectAll("tr").data(rows.slice(1))
+        .enter().append("tr")
+        .selectAll("td")
+        .data(function(d){return d;})
+        .enter().append("td")
+        .text(function(d){return d;})
+});
--- html/js/plugins.js
+++ html/js/plugins.js
@@ -26,4 +26,44 @@
 $(document).ready(function() {
 	// wow animation
 	new WOW().init();
+	// add sorting and responsiveness to data table
+	var checkExist = setInterval(function() {
+	   if ($('#data_grid table').length) {
+		  $("#data_grid table").addClass("tablesorter table grid footable"); 
+		  $("#data_grid th").addClass("pressed"); 
+		  $('#data_grid th').attr('data-hide','phone,tablet');
+		  $('#data_grid th:first-of-type').attr('data-hide','');
+		  $('#data_grid th:nth-of-type(2)').attr('data-hide','');
+		  $("#data_grid table").tablesorter();
+		  
+		  $("#data_grid tr").each(function () {
+
+			  $('td:contains("http://")', this).each(function () {
+				  var imgLnk = $(this).text();
+				  $(this).html("<img src='" + imgLnk + "'>");
+			   })
+			   
+			  $('td:contains(".com")', this).each(function () {
+				  var siteLnk = $(this).text();
+				  $(this).html("<a target='_blank' href='http://" + siteLnk + "'>" + siteLnk + "</a>");
+			   })
+			   
+			   $('#data_grid th:first-of-type').html("#");
+			   $('#data_grid td:first-of-type').html("<a href='javascript:void(0);'><i class='fa fa-eye'></i></a>");
+		  })
+		  
+		  // responsive data table
+		  $('.footable').footable({
+			calculateWidthOverride: function() {
+			  return { width: $(window).width() };
+			}
+		  });
+		  if (!$('.footable').is(':visible')) {
+		  	return;
+		  }
+		  $('.footable').trigger('footable_initialize');
+		  
+		  clearInterval(checkExist);
+	   }
+	}, 100);		
 });
